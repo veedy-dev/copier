6140756a5decd532051af28e2c12efcdb480399b
Veedy <38901140+veedy-dev@users.noreply.github.com>
2022-06-22 17:11:58 +0700

    changed folders path

---
+++ sync.bat
@@ -0,0 +1,5 @@
+ECHO ON
+ECHO Copying BP & RP to Repository
+REM A batch script to execute a Python script
+SET PATH=%PATH%;D:\Project\Python\copier
+python sync.py
--- sync.py
+++ sync.py
@@ -2,9 +2,9 @@ from distutils.dir_util import copy_tree
 import os
 import shutil
 
-dirs = ["D:/Minecraft Stuff/Addons Development/survival-expansion-ocean/Survival Expansion Ocean BP", "D:/Minecraft Stuff/Addons Development/survival-expansion-ocean/Survival Expansion Ocean RP"]
-BP = "D:/Minecraft Stuff/Tools/bridge. v2/projects/Survival Expansion Ocean/BP"
-RP = "D:/Minecraft Stuff/Tools/bridge. v2/projects/Survival Expansion Ocean/RP"
+dirs = ["D:/Minecraft Stuff/Addons Development/survival-expansion-ocean/Survival Expansion Ocean BP/", "D:/Minecraft Stuff/Addons Development/survival-expansion-ocean/Survival Expansion Ocean RP/"]
+BP = "D:/Minecraft Stuff/Tools/bridge. v2/projects/Survival Expansion Ocean/BP/"
+RP = "D:/Minecraft Stuff/Tools/bridge. v2/projects/Survival Expansion Ocean/RP/"
 
 shutil.rmtree(BP)
 shutil.rmtree(RP)
