d82f8fd8e2d2ad2bd621ccdb69665507eed57044
Veedy <38901140+veedy-dev@users.noreply.github.com>
2022-06-22 20:50:10 +0700

    Implemented Json config generator

--- autocopy.py
+++ autocopy.py
@@ -9,6 +9,7 @@ import os
 import shutil
 from distutils.dir_util import copy_tree
 
+
 def copyBP(srcBP, dstBP):
     if os.path.exists(srcBP) and os.path.exists(dstBP):
         if os.listdir(dstBP) == []:
@@ -17,6 +18,8 @@ def copyBP(srcBP, dstBP):
             shutil.rmtree(dstBP)
             copy_tree(srcBP, dstBP)
         print("BP files copied successfully")
+    else:
+        print("BP Source or destination folder does not exist")
 
 
 def copyRP(srcRP, dstRP):
@@ -31,7 +34,39 @@ def copyRP(srcRP, dstRP):
         print("RP Source or destination folder does not exist")
 
 
-copyBP(input("Enter source BP folder: "),
-       input("Enter destination BP folder: "))
-copyRP(input("Enter source RP folder: "),
-       input("Enter destination RP folder: "))
+def savePaths():
+    srcBP = input("Enter source BP folder: ")
+    dstBP = input("Enter destination BP folder: ")
+    srcRP = input("Enter source RP folder: ")
+    dstRP = input("Enter destination RP folder: ")
+    # replace all backslashes with forward slashes
+    srcBP = srcBP.replace("\\", "/")
+    dstBP = dstBP.replace("\\", "/")
+    srcRP = srcRP.replace("\\", "/")
+    dstRP = dstRP.replace("\\", "/")
+
+    with open("paths.json", "w") as f:
+        f.write("{\"srcBP\":\"" + srcBP + "\",\"dstBP\":\"" + dstBP +
+                "\",\"srcRP\":\"" + srcRP + "\",\"dstRP\":\"" + dstRP + "\"}")
+        f.close()
+    print("Paths saved successfully")
+
+
+if os.path.exists("paths.json"):
+    with open("paths.json", "r") as f:
+        data = f.read()
+        
+        srcBP = data[data.find("srcBP") + 8:data.find("dstBP") - 1]
+        dstBP = data[data.find("dstBP") + 8:data.find("srcRP") - 1]
+        srcRP = data[data.find("srcRP") + 8:data.find("dstRP") - 1]
+        dstRP = data[data.find("dstRP") + 8:data.find("}") - 1]
+
+        # remove comma from the paths
+        srcBP = srcBP.replace(",", "")
+        dstBP = dstBP.replace(",", "")
+        srcRP = srcRP.replace(",", "")
+        dstRP = dstRP.replace(",", "")
+        
+        print(srcBP, dstBP, srcRP, dstRP)
+else:
+    savePaths()
