35f9d5bee6809d2d9ac07c1b3379ee0f2aa03445
Veedy <38901140+veedy-dev@users.noreply.github.com>
2022-06-22 17:11:45 +0700

    added autocopy bat

---
+++ autocopy.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set BP=D:\Minecraft Stuff\Tools\bridge. v2\projects\Survival Expansion Ocean\BP
+set dstBP=D:\dummy\BP
+set RP=D:\Minecraft Stuff\Tools\bridge. v2\projects\Survival Expansion Ocean\RP
+set dstRP=D:\dummy\RP
+python autocopy.py
